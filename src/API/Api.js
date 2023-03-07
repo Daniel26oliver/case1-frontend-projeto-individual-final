16e2ad38e53692db5dad27b809e201dd8d865ba4
Daniel26oliver <109106383+Daniel26oliver@users.noreply.github.com>
2023-03-07 14:00:57 -0300

    Update Api.js

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,39 +1,39 @@
 const Api = () => {
-  const url = 'https://lista-filmes.onrender.com';
+  const url = 'https://lista-livros.onrender.com';
 
   return {
     FILMES_GET() {
-      return fetch(`${url}/filmes`);
+      return fetch(`${url}/livro`);
     },
-    FILME_POST(titulo, duracao, genero) {
-      return fetch(`${url}/filme`, {
+    FILME_POST(titulo, genero, categoria) {
+      return fetch(`${url}/livro`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           titulo,
-          duracao,
           genero,
+          categoria,
         }),
       }).then((res) => console.log(res.status));
     },
-    FILME_PUT(titulo, duracao, genero, id) {
-      return fetch(`${url}/filme`, {
+    FILME_PUT(titulo, genero, categoria, id) {
+      return fetch(`${url}/livro`, {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           titulo,
-          duracao,
           genero,
+          categoria,
           id,
         }),
       }).then((res) => console.log(res.status));
     },
     FILME_DELETE(id) {
-      return fetch(`${url}/filme`, {
+      return fetch(`${url}/livro`, {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json',
